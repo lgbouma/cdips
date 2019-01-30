a5c27f563173464af8837207f1ff12d01634594d
lgbouma <lgbouma@mit.edu>
2019-01-29 19:13:20 -0500

    update EPD parameter plots
    timeseries, and magnitude correlation

--- src/plot_EPD_parameters_vs_time.py
+++ src/plot_EPD_parameters_vs_time.py
@@ -1,6 +1,5 @@
 """
-scatter plots of magnitudes vs various parameters... to see whether the EPD
-linear fitting approach works.
+(mag, T, x, y, s, d, k, bgd) vs time
 """
 import numpy as np, matplotlib.pyplot as plt, pandas as pd
 from glob import glob
@@ -12,84 +11,10 @@ from bspline_fit_xyTmag_BIC_approach import homog_get_data
 
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-def plot_flux_vs_EPD_param(lcpaths, lcdatalist, magtype='IRM1',
-                                frac_of_lc=1.0, savdir=None):
-
-    xcols = ['TMID_BJD','XIC','YIC','CCDTEMP','FSV','FDV','FKV','BGV']
-    xkeys = ['tmidbjd','x','y','T','s','d','k','bkgd']
-
-    for lcpath, lc in zip(lcpaths, lcdatalist):
-
-        savname = '{}_vs_EPDparams_frac{:.1f}_{}.png'.format(
-            magtype, frac_of_lc, os.path.splitext(os.path.basename(lcpath))[0])
-
-        savpath = os.path.join(savdir, savname)
-
-        if os.path.exists(savpath):
-            print('found {}, continue'.format(savpath))
-            continue
-
-        time = lc['TMID_UTC']
-
-        flux = lc[magtype]
-        timeind = np.argsort(time)
-        d = {}
-        for xcol, k in zip(xcols, xkeys):
-            # pandas/seaborn wants little-endian
-            le_array = lc[xcol].byteswap().newbyteorder()
-            # sort everything by time
-            time_sorted_arr = le_array[timeind]
-            # take the cut so that you deal with orbit-specific data, if
-            # desired
-            d[k] = time_sorted_arr[:int(frac_of_lc*len(time_sorted_arr))]
-
-        d[magtype] = flux[timeind][:int(frac_of_lc*len(time_sorted_arr))]
-
-        df = pd.DataFrame(d)
-
-        if np.all(pd.isnull(df[magtype])):
-            print('mags are all NaN for {}, continue'.format(savname))
-            continue
-
-        # PLOT flux vs each xcol.
-        plt.close('all')
-
-        fig,axs = plt.subplots(nrows=len(xcols), ncols=1, figsize=(6,6))
-        axs = axs.flatten()
-
-        cm = plt.cm.get_cmap('viridis')
-
-        timeval = np.array(df['tmidbjd'])
-        for ax, xkey in zip(axs, xkeys):
-            xval = df[xkey]
-            yval = df[magtype]
-
-            cs = ax.scatter(xval, yval, rasterized=True, alpha=0.8, zorder=3,
-                            c=timeval, lw=0, s=3, cmap=cm)
-
-            ax.set_ylabel(magtype+' vs '+xkey, fontsize='xx-small')
-
-            ax.get_yaxis().set_tick_params(which='both', direction='in')
-            ax.get_xaxis().set_tick_params(which='both', direction='in')
-            ax.xaxis.set_tick_params(labelsize='xx-small')
-            ax.yaxis.set_tick_params(labelsize='xx-small')
-
-        cbar_ax = fig.add_axes([1., 0.15, 0.03, 0.7])
-        cbar = fig.colorbar(cs, cax=cbar_ax, pad=0.01, use_gridspec=True)
-        cbar_ax.yaxis.set_tick_params(labelsize='xx-small')
-
-        cbar.ax.tick_params(direction='in')
-        cbar.ax.set_ylabel('Time (BJD)', fontsize='xx-small')
-
-        fig.tight_layout(h_pad=-1, w_pad=-1)
-        fig.savefig(savpath, dpi=400, bbox_inches='tight')
-        print('made {}'.format(savpath))
-
-
 def plot_EPD_parameters_vs_time(lcpaths, lcdatalist, magtype='IRM1',
                                 frac_of_lc=1.0, savdir=None):
 
-    xcols = ['TMID_UTC','XIC','YIC','CCDTEMP','FSV','FDV','FKV','BGV']
+    xcols = ['TMID_BJD','XIC','YIC','CCDTEMP','FSV','FDV','FKV','BGV']
     xkeys = ['tmidbjd','x','y','T','s','d','k','bkgd']
 
     for lcpath, lc in zip(lcpaths, lcdatalist):
@@ -103,7 +28,7 @@ def plot_EPD_parameters_vs_time(lcpaths, lcdatalist, magtype='IRM1',
             print('found {}, continue'.format(savpath))
             continue
 
-        time = lc['TMID_UTC']
+        time = lc['TMID_BJD']
 
         flux = lc[magtype]
         timeind = np.argsort(time)
@@ -132,26 +57,42 @@ def plot_EPD_parameters_vs_time(lcpaths, lcdatalist, magtype='IRM1',
                                sharex=True)
         axs = axs.flatten()
 
-        timeval = np.array(df['tmidbjd'])
+        xoffset = int(np.median(df['tmidbjd']))
+        xval = df['tmidbjd'] - xoffset
+
         for ax, xkey in zip(axs, xkeys):
             if xkey=='tmidbjd':
                 xkey = magtype
             yval = df[xkey]
-            xval = df['tmidbjd']
+
+            if xkey in ['x','y']:
+                yoffset = int(np.mean(yval))
+                yval -= yoffset
+                xkey += '- {:d}'.format(yoffset)
+            elif xkey in [magtype]:
+                yoffset = np.round(np.median(yval), decimals=1)
+                yval -= yoffset
+                yval *= 1e3
+                xkey += '- {:.1f} [mmag]'.format(yoffset)
 
             ax.scatter(xval, yval, rasterized=True, alpha=0.8, zorder=3, c='k',
                        lw=0, s=3)
 
             ax.set_ylabel(xkey, fontsize='xx-small')
 
+            #ax.xaxis.set_ticks_position('both')
             ax.get_yaxis().set_tick_params(which='both', direction='in')
             ax.get_xaxis().set_tick_params(which='both', direction='in')
             ax.xaxis.set_tick_params(labelsize='xx-small')
             ax.yaxis.set_tick_params(labelsize='xx-small')
 
-        ax.set_xlabel('BJD')
+            if magtype in xkey:
+                ylim = ax.get_ylim()
+                ax.set_ylim((max(ylim), min(ylim)))
 
-        fig.tight_layout(h_pad=-1, w_pad=-1)
+        ax.set_xlabel('BJD$_{\mathrm{TDB}}$ - '+'{}'.format(xoffset))
+
+        fig.tight_layout(h_pad=-1)
         fig.savefig(savpath, dpi=400, bbox_inches='tight')
         print('made {}'.format(savpath))
 
@@ -160,21 +101,16 @@ def plot_EPD_parameters_vs_time(lcpaths, lcdatalist, magtype='IRM1',
 
 if __name__=="__main__":
 
-    #for lctype in ['corner','center']:
-    for lctype in ['center']:
+    for lctype in ['center','corner']:
 
-        savdir = '../results/projid1030_lc_fit_check/{}_lcs/'.format(lctype)
+        savdir = '../results/projid1088_cam2_ccd2_lcs/{}_lcs/'.format(lctype)
+        lcdir = savdir
 
-        lcpaths, lcdatalist =  homog_get_data(lctype=lctype)
+        lcpaths, lcdatalist = homog_get_data(lctype=lctype, lcdir=lcdir)
 
         plot_EPD_parameters_vs_time(lcpaths, lcdatalist, magtype='IRM1',
                                     frac_of_lc=1.0, savdir=savdir)
 
-        plot_EPD_parameters_vs_time(lcpaths, lcdatalist, magtype='IRM1',
-                                    frac_of_lc=0.4, savdir=savdir)
+        #plot_EPD_parameters_vs_time(lcpaths, lcdatalist, magtype='IRM1',
+        #                            frac_of_lc=0.4, savdir=savdir)
 
-        plot_flux_vs_EPD_param(lcpaths, lcdatalist, magtype='IRM1',
-                               frac_of_lc=0.4, savdir=savdir)
-
-        #FIXME: you want this to by X vs time, Y vs time, etc.... not the plot
-        #that you have made!!!
--- src/plot_mag_vs_EPD_parameters.py
+++ src/plot_mag_vs_EPD_parameters.py
@@ -1,6 +1,6 @@
 """
-scatter plots of magnitudes vs various parameters... to see whether the EPD
-linear fitting approach works.
+magnitude vs (time, T, x, y, s, d, k, bgd). for: single stars. and 4 chosen
+stars (for paper).
 """
 import numpy as np, matplotlib.pyplot as plt, pandas as pd
 from glob import glob
@@ -8,123 +8,300 @@ import os
 
 from astropy.io import fits
 
-def get_data(n_lcs=10):
-    # return list of length n_lcs, where each element is a FITS_rec table
-    # containing all the needed information.
+from bspline_fit_xyTmag_BIC_approach import homog_get_data
 
-    lcdir = '../data/lightcurves/'
-    lcfiles = glob(os.path.join(lcdir, '*_llc.fits'))
+from astrobase import lcmath
 
-    if len(lcfiles)>n_lcs:
-        lcfiles = np.random.choice(lcfiles, size=n_lcs, replace=False)
-    else:
-        pass
+def plot_mag_vs_EPD_parameters(lcpaths, lcdatalist, magtype='IRM1',
+                               get_norbits=1, expected_norbits=2, savdir=None):
 
-    datalist = []
-    for lcfile in lcfiles:
-        hdulist = fits.open(lcfile)
-        datalist.append(hdulist[1].data)
+    xcols = ['TMID_BJD','CCDTEMP','XIC','YIC','FSV','FDV','FKV']
+    xkeys = ['tmidbjd','T','x','y','s','d','k']
+    # could also include other parameters, like decimal_x, decimal_y,
+    # cross-terms...
+    desiredx = xkeys
 
-    return lcfiles, datalist
-
-
-def make_scatter_plots(lcpaths, lcdatalist, magtype='IRM1'):
-
-    xcols = ['FSV','FDV','FKV','XIC','YIC','CCDTEMP']
-    xkeys = ['s','d','k','x','y','T']
-    desiredx = ['s', 'd', 'k', 'x', 'y', 'T', 'decimal_x', 'decimal_y',
-                'sin(2pi*x)', 'sin(2pi*y)', 'cos(2pi*x)', 'cos(2pi*y)', 's^2',
-                'd^2', 'k^2', 's*d', 'd*k', 's*k']
+    if get_norbits not in [1]:
+        raise AssertionError('you usually only want to get 1 orbit')
 
     for lcpath, lc in zip(lcpaths, lcdatalist):
 
+        # collect the data for the first orbit in this sector
+        times = lc['TMID_BJD']
+
+        orbitgap = 1. # days
+        found_norbits, groups = lcmath.find_lc_timegroups(times, mingap=orbitgap)
+
+        if found_norbits != expected_norbits:
+            outmsg = (
+                'assuming given two orbits. {} orbits. Time {}'.
+                format(norbits, repr(times))
+            )
+            raise AssertionError(outmsg)
+
+        if get_norbits==1:
+            group = groups[0]
+        else:
+            raise NotImplementedError
+
+        # collect the data for the first orbit in this sector
         d = {}
         for xcol, k in zip(xcols, xkeys):
-            d[k] = lc[xcol]
+            d[k] = lc[xcol][group]
 
-        for xstr in desiredx:
+        if np.all(pd.isnull(lc[magtype])):
+            print('mags are all NaN for {}, continue'.format(savname))
+            continue
 
-            savdir = '../results/epd_parameter_selection/'
-            savname = '{}_vs_{}_{}.png'.format(
-                magtype, xstr, os.path.basename(lcpath))
-            savpath = os.path.join(savdir, savname)
+        ##########################################
 
-            if os.path.exists(savpath):
-                print('found {}, continue'.format(savpath))
-                continue
+        # begin plotting.
+        nrows = len(xcols)
 
-            plt.close('all')
-            f,ax = plt.subplots(figsize=(4,3))
+        plt.close('all')
+        fig,axs = plt.subplots(nrows=nrows, ncols=1, figsize=(3,2*nrows))
+        axs = axs.flatten()
 
-            yval = lc[magtype]
+        yval = lc[magtype][group]
+        yoffset = np.round(np.median(yval),decimals=2)
+        yval -= yoffset
+        yval *= 1e3
 
-            if xstr in ['s','d','k','x','y','T']:
+        savname = ('{}_vs_EPD_parameters_{}_norbit{}.png'.
+                   format(magtype,
+                          os.path.basename(lcpath).replace('.fits',''),
+                          get_norbits)
+        )
+        savpath = os.path.join(savdir, savname)
+
+        if os.path.exists(savpath):
+            print('found {}, continue'.format(savpath))
+            continue
+
+        for xstr, ax in zip(desiredx, axs):
+
+            if xstr in ['s','d','k','x','y','T','tmidbjd']:
                 xval = d[xstr]
-
             elif xstr == 'decimal_x':
                 xval = d['x'] - np.floor(d['x'])
-
             elif xstr == 'decimal_y':
                 xval = d['y'] - np.floor(d['y'])
-
             elif xstr == 'sin(2pi*x)':
                 xval = np.sin(2*np.pi*d['x'])
-
             elif xstr == 'sin(2pi*y)':
                 xval = np.sin(2*np.pi*d['y'])
-
             elif xstr == 'cos(2pi*x)':
                 xval = np.cos(2*np.pi*d['x'])
-
             elif xstr == 'cos(2pi*y)':
                 xval = np.cos(2*np.pi*d['y'])
-
             elif xstr == 's^2':
                 xval = d['s']*d['s']
-
             elif xstr == 'd^2':
                 xval = d['d']*d['d']
-
             elif xstr == 'k^2':
                 xval = d['k']*d['k']
-
             elif xstr == 's*d':
                 xval = d['s']*d['d']
-
             elif xstr == 'd*k':
                 xval = d['d']*d['k']
-
             elif xstr == 's*k':
                 xval = d['s']*d['k']
 
-            ax.scatter(xval, yval, s=15, alpha=0.8, lw=0)
-            ax.set_xlabel(xstr)
-            ax.set_ylabel(magtype)
+            if xstr == 'tmidbjd':
+                xval -= np.median(xval)
+            if xstr in ['x','y']:
+                xoffset = int(np.median(xval))
+                xval -= xoffset
+
+            if xstr == 'tmidbjd':
+                ax.scatter(xval, yval, rasterized=True, alpha=0.8, zorder=3,
+                           c='k', lw=0, s=5)
+            else:
+                ax.scatter(xval, yval, rasterized=True, alpha=0.8, zorder=3,
+                           c='k', lw=0, s=4)
+
+            if xstr == 'tmidbjd':
+                xstr = 't - t$_0$ [d]'
+            if xstr in ['x','y']:
+                xstr = xstr + ' - {}'.format(xoffset)
+            ax.text(
+                0.98, 0.02, xstr, fontsize='x-small',
+                ha='right',va='bottom',
+                transform=ax.transAxes
+            )
+            ax.set_ylabel('{} - {} [mmag]'.format(magtype, yoffset),
+                          fontsize='x-small')
 
             ax.get_yaxis().set_tick_params(which='both', direction='in')
             ax.get_xaxis().set_tick_params(which='both', direction='in')
 
-            ax.set_ylim(
-                [np.mean(yval)-3*np.std(yval),
-                 np.mean(yval)+3*np.std(yval) ]
-            )
-            ax.set_xlim(
-                [np.mean(xval)-3*np.std(xval),
-                 np.mean(xval)+3*np.std(xval) ]
-            )
+            ylim = ax.get_ylim()
+            ax.set_ylim((max(ylim), min(ylim)))
 
-            f.tight_layout()
-            f.savefig(savpath, bbox_inches='tight', dpi=400)
+        fig.tight_layout(h_pad=0.15)
+        fig.savefig(savpath, dpi=400, bbox_inches='tight')
 
-            print('made {}'.format(savpath))
+        print('made {}'.format(savpath))
+
+
+def plot_mag_vs_EPD_parameters_four_stars(magtype='IRM1', get_norbits=1,
+                                          expected_norbits=2, savdir=None):
+    """
+    for 4 chosen stars, do columns of their flux vs EPD parameters (x,y,T,
+    etc).
+
+    two left stars are selected to be representative of center of field.
+    two right stars are selected to be representative of corner of field.
+    """
+
+    # get the lcpaths and data list for the selected stars
+
+    # brightish, center
+    # IRM1_vs_EPD_parameters_4979631232408118784_llc_norbit1.png
+    # IRM1_vs_EPD_parameters_4979603607178484608_llc_norbit1.png
+    # ditto, corner
+    # IRM1_vs_EPD_parameters_4955130505568582528_llc_norbit1.png
+    # IRM1_vs_EPD_parameters_4955138064711041280_llc_norbit1.png
+
+    centerdir = '../results/projid1088_cam2_ccd2_lcs/center_lcs/'
+    cornerdir = '../results/projid1088_cam2_ccd2_lcs/corner_lcs/'
+    lcpaths = [
+        os.path.join(centerdir,'4979631232408118784_llc.fits'),
+        os.path.join(centerdir,'4979603607178484608_llc.fits'),
+        os.path.join(cornerdir,'4955130505568582528_llc.fits'),
+        os.path.join(cornerdir,'4955138064711041280_llc.fits'),
+    ]
+
+    lcdatalist = []
+    for lcfile in lcpaths:
+        hdulist = fits.open(lcfile)
+        lcdatalist.append(hdulist[1].data)
+        hdulist.close()
+
+    xcols = ['TMID_BJD','CCDTEMP','XIC','YIC','FSV','FDV','FKV']
+    xkeys = ['tmidbjd','T','x','y','s','d','k']
+    desiredx = xkeys
+
+    if get_norbits not in [1]:
+        raise AssertionError('you only want to get 1 orbit')
+
+    plt.close('all')
+    nrows = len(xcols)
+    ncols = len(lcpaths)
+    fig,axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(3*ncols,2*nrows))
+
+    savname = ('{}_vs_EPD_parameters_fourstars_norbit{}.png'.
+               format(magtype,
+                      get_norbits)
+    )
+    savpath = os.path.join(savdir, savname)
+
+    if os.path.exists(savpath):
+        print('found {}, quitting'.format(savpath))
+        return 0
+
+    # iterate over columns
+    for lcpath, lc, colaxs in zip(lcpaths, lcdatalist, axs.T):
+
+        # collect the data for the first orbit in this sector
+        times = lc['TMID_BJD']
+
+        orbitgap = 1. # days
+        found_norbits, groups = lcmath.find_lc_timegroups(times, mingap=orbitgap)
+
+        if found_norbits != expected_norbits:
+            outmsg = (
+                'assuming given two orbits. {} orbits. Time {}'.
+                format(norbits, repr(times))
+            )
+            raise AssertionError(outmsg)
+
+        if get_norbits==1:
+            group = groups[0]
+        else:
+            raise NotImplementedError
+
+        # collect the data for the first orbit in this sector
+        d = {}
+        for xcol, k in zip(xcols, xkeys):
+            d[k] = lc[xcol][group]
+
+        if np.all(pd.isnull(lc[magtype])):
+            print('mags are all NaN for {}, continue'.format(savname))
+            continue
+
+        ##########################################
+
+        # begin plotting.
+        yval = lc[magtype][group]
+        yoffset = np.round(np.median(yval),decimals=2)
+        yval -= yoffset
+        yval *= 1e3
+
+        for xstr, ax in zip(desiredx, colaxs):
+
+            if xstr in ['s','d','k','x','y','T','tmidbjd']:
+                xval = d[xstr]
+
+            if xstr == 'tmidbjd':
+                xval -= np.median(xval)
+            if xstr in ['x','y']:
+                xoffset = int(np.median(xval))
+                xval -= xoffset
+
+            if xstr == 'tmidbjd':
+                ax.scatter(xval, yval, rasterized=True, alpha=0.8, zorder=3,
+                           c='k', lw=0, s=5)
+            else:
+                ax.scatter(xval, yval, rasterized=True, alpha=0.8, zorder=3,
+                           c='k', lw=0, s=4)
+
+            if xstr == 'tmidbjd':
+                xstr = 't - t$_0$ [d]'
+            if xstr in ['x','y']:
+                xstr = xstr + ' - {}'.format(xoffset)
+            ax.text(
+                0.98, 0.02, xstr, fontsize='x-small',
+                ha='right',va='bottom',
+                transform=ax.transAxes
+            )
+            ax.set_ylabel('{} - {} [mmag]'.format(magtype, yoffset),
+                          fontsize='x-small')
+
+            ax.get_yaxis().set_tick_params(which='both', direction='in')
+            ax.get_xaxis().set_tick_params(which='both', direction='in')
+
+            ylim = ax.get_ylim()
+            ax.set_ylim((max(ylim), min(ylim)))
+
+    fig.tight_layout(h_pad=0.15)
+    fig.savefig(savpath, dpi=400, bbox_inches='tight')
+
+    print('made {}'.format(savpath))
 
 
 if __name__=="__main__":
 
+    makerandomlcplots = 0
+    makefourstarlcplot = 1
+
     np.random.seed(42)
 
-    n_lcs = 10
+    if makerandomlcplots:
 
-    lcpaths, lcdatalist = get_data(n_lcs=n_lcs)
+        for lctype in ['center','corner']:
 
-    make_scatter_plots(lcpaths, lcdatalist)
+            savdir = '../results/projid1088_cam2_ccd2_lcs/{}_lcs/'.format(lctype)
+            lcdir = savdir
+
+            lcpaths, lcdatalist = homog_get_data(lctype=lctype, lcdir=lcdir)
+
+            plot_mag_vs_EPD_parameters(lcpaths, lcdatalist, magtype='IRM1',
+                                       get_norbits=1, expected_norbits=2,
+                                       savdir=savdir)
+
+    if makefourstarlcplot:
+        plot_mag_vs_EPD_parameters_four_stars(
+            magtype='IRM1', get_norbits=1, expected_norbits=2,
+            savdir='../results/projid1088_cam2_ccd2_lcs'
+        )
