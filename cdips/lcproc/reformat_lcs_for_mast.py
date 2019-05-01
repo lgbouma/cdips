9909c561c09ed353e6d513c3f3c68287808e3cd4
lgbouma <lgbouma@mit.edu>
2019-05-01 17:18:58 -0400

    reformat LCs for MAST: update per Fleming rev ver1
    AKA: we're a good deal closer to having a HLSP

--- cdips/lcproc/reformat_lcs_for_mast.py
+++ cdips/lcproc/reformat_lcs_for_mast.py
@@ -19,7 +19,7 @@ def _map_timeseries_key_to_comment(k):
         "rstfc" : "Unique frame key",
         "starid": "GAIA ID of the object",
         "xcc"   : "Original X coordinate on CCD on photref frame",
-        "ycc"   : "Original y coordinate on CCD on photref frame",
+        "ycc"   : "Original Y coordinate on CCD on photref frame",
         "xic"   : "Shifted X coordinate on CCD on subtracted frame",
         "yic"   : "Shifted Y coordinate on CCD on subtracted frame",
         "fsv"   : "Measured S value (see Pal 2009 eq 31)",
@@ -59,7 +59,55 @@ def _map_timeseries_key_to_comment(k):
     return kcd[k]
 
 
-def _reformat_header(lcpath, cdips_df, outdir):
+def _map_timeseries_key_to_unit(k):
+    kcd = {
+        "tmid_utc": "day",
+        "tmid_bjd": "day",
+        "bjdcorr": "day",
+        "rstfc" : "unitless",
+        "starid": "unitless",
+        "xcc"   : "pixels",
+        "ycc"   : "pixels",
+        "xic"   : "pixels",
+        "yic"   : "pixels",
+        "fsv"   : "unitless",
+        "fdv"   : "unitless",
+        "fkv"   : "unitless",
+        "bgv"   : "ADU",
+        "bge"   : "ADU",
+        "ifl1"  : "ADU",
+        "ife1"  : "ADU",
+        "irm1"  : "mag",
+        "ire1"  : "mag",
+        "irq1"  : "unitless",
+        "ifl2"  : "ADU",
+        "ife2"  : "ADU",
+        "irm2"  : "mag",
+        "ire2"  : "mag",
+        "irq2"  : "unitless",
+        "ifl3"  : "ADU",
+        "ife3"  : "ADU",
+        "irm3"  : "mag",
+        "ire3"  : "mag",
+        "irq3"  : "unitless",
+        'tfa1'  : "mag",
+        'tfa2'  : "mag",
+        'tfa3'  : "mag",
+        "ccdtemp" : "degcelcius",
+        "ntemps"  : "unitless",
+        'dtr_isub': "bool",
+        'dtr_epd' : "bool",
+        'dtr_tfa' : "bool",
+        'projid' :  "unitless",
+        'btc_ra' : "deg",
+        'btc_dec' : "deg",
+        'rdistpx': "pixels",
+        'thetadeg': "degrees"
+    }
+    return kcd[k]
+
+
+def _reformat_header(lcpath, cdips_df, outdir, sectornum, cdipsvnum):
 
     hdul = fits.open(lcpath)
     primaryhdr, hdr, data = (
@@ -79,39 +127,129 @@ def _reformat_header(lcpath, cdips_df, outdir):
 
     primaryhdr.set('CDCLSTER',
                    info['cluster'].iloc[0],
-                   'Name(s) of cluster in CDIPSREF [,sep]')
+                   'Cluster name(s) in CDIPSREF [,sep]')
 
     primaryhdr.set('CDEXTCAT',
                    info['ext_catalog_name'].iloc[0],
-                   'Name(s) of star in CDIPSREF [,sep]')
+                   'Star name(s) in CDIPSREF [,sep]')
 
     primaryhdr.set('CDXMDIST',
                    str(info['dist'].iloc[0]),
-                   '[deg] dist btwn CDIPSREF & GAIADR2 locn')
+                   '[deg] DIST btwn CDIPSREF & GAIADR2 locn')
+
+    #
+    # info for MAST
+    #
+    toremove = ['TSTOP', 'TSTART', 'DATE-END', 'DATE-OBS', 'TELAPSE',
+                'LIVETIME', 'EXPOSURE',
+                'DEADC', 'BJDREFF']
+
+    for _toremove in toremove:
+        primaryhdr.remove(_toremove)
 
     primaryhdr.set('TIMEUNIT',
                    primaryhdr['TIMEUNIT'],
                    'Time unit for TMID_BJD')
 
-    toremove = ['TSTOP', 'TSTART', 'DATE-END', 'DATE-OBS', 'TELAPSE',
-                'LIVETIME']
+    primaryhdr.set('BJDREFF',
+                   0,
+                   'fraction of the day in BTJD reference date')
+
+    primaryhdr.set('BJDREFI',
+                   0,
+                   'integer part of BTJD reference date')
+
+    primaryhdr.set('FILTER',
+                   'TESS',
+                   'MAST HLSP required keyword')
+
+    primaryhdr.set('OBJECT',
+                   int(lcgaiaid),
+                   'Gaia DR2 source_id')
+
+    primaryhdr.set('SECTOR',
+                   int(sectornum),
+                   'Observing sector')
+
+    primaryhdr.set('RADESYS',
+                   'ICRS',
+                   'reference frame of celestial coordinates')
+
+    primaryhdr.set('EQUINOX',
+                   2015.5,
+                   'equinox of celestial coordinate system')
+
+    primaryhdr.set('RA_OBJ',
+                   primaryhdr['RA[deg]'],
+                   '[deg] right ascension')
+
+    primaryhdr.set('DEC_OBJ',
+                   primaryhdr['Dec[deg]'],
+                   '[deg] declination')
+
+    toremove = ['RA[deg]', 'Dec[deg]']
     for _toremove in toremove:
         primaryhdr.remove(_toremove)
+
 
     primaryhdr.set('Gaia-ID',
                    primaryhdr['Gaia-ID'],
                    'GaiaDR2 source_id. ->lum_val from same')
 
     #
-    # set timeseries extension header key comments
+    # TICv8 xmatch info, once it exists...
+    #
+    primaryhdr.set('TICVER',
+                   8,
+                   'TIC version')
+
+    primaryhdr.set('TESSMAG',
+                   42, #FIXME
+                   '[mag] TESS magnitude')
+
+    primaryhdr.set('TICCONT',
+                   42, #FIXME
+                   'ContamRatio from TIC')
+
+    #
+    # who dun it
+    #
+    primaryhdr.set('ORIGIN',
+                   'Luke Bouma / CDIPS',
+                   'Author / project name.')
+
+    #
+    # set timeseries extension header key comments. also set the units.
     #
     tfields = hdr['TFIELDS'] # number of table fields
     hdrkv = {}
     for ind in range(1,tfields+1):
         key = 'TTYPE{}'.format(ind)
         hdrkv[key] = hdr[key]
+
     for k,v in hdrkv.items():
+
         hdr.comments[k] = _map_timeseries_key_to_comment(v.lower())
+
+        hdr[k.replace('TTYPE','TUNIT')] = (
+            _map_timeseries_key_to_unit( v.lower())
+        )
+
+    hdr.set(
+        'MJD_BEG',
+        np.nanmin(data['TMID_UTC'] - 2400000.5),
+        'min(TMID_UTC) - 2400000.5'
+    )
+    hdr.set(
+        'MJD_END',
+        np.nanmax(data['TMID_UTC'] - 2400000.5),
+        'max(TMID_UTC) - 2400000.5'
+    )
+    hdr.set(
+        'XPOSURE',
+        1800,
+        '[sec] exposure time per cadence'
+    )
 
     #
     # write it (!)
@@ -121,7 +259,17 @@ def _reformat_header(lcpath, cdips_df, outdir):
     timeseries_hdu.header = hdr
     outhdulist = fits.HDUList([primary_hdu, timeseries_hdu])
 
-    outfile = os.path.join(outdir, os.path.basename(lcpath))
+    outname = (
+        'hlsp_cdips_tess_ffi_'
+        'gaiatwo{zsourceid}-{zsector}_'
+        'tess_v{zcdipsvnum}_llc.fits'
+    ).format(
+        zsourceid=str(lcgaiaid).zfill(22),
+        zsector=str(sectornum).zfill(4),
+        zcdipsvnum=str(cdipsvnum).zfill(2)
+    )
+
+    outfile = os.path.join(outdir, outname)
 
     try:
         outhdulist.writeto(outfile, overwrite=False)
@@ -133,14 +281,14 @@ def _reformat_header(lcpath, cdips_df, outdir):
     outhdulist.close()
 
 
-def reformat_headers(lcpaths, outdir):
+def reformat_headers(lcpaths, outdir, sectornum, cdipsvnum):
 
     cdips_df = pd.read_csv(cdips_cat_file, sep=';')
 
     for lcpath in lcpaths:
         outpath = os.path.join(outdir, os.path.basename(lcpath))
         if not os.path.exists(outpath):
-            _reformat_header(lcpath, cdips_df, outdir)
+            _reformat_header(lcpath, cdips_df, outdir, sectornum, cdipsvnum)
 
 
 def mask_orbit_start_and_end(lcpaths):
