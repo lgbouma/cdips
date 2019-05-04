f4376ea9745b00468ecef56f8abb9b8759a6504e
lgbouma <lgbouma@mit.edu>
2019-05-04 17:39:56 -0400

    vetting: add cluster properties page

--- drivers/make_vetting_multipg_pdf.py
+++ drivers/make_vetting_multipg_pdf.py
@@ -14,7 +14,7 @@ from astropy.io import fits
 from datetime import datetime
 
 def make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf, sourceid,
-                             suppdf,
+                             supprow, suppfulldf,
                              mask_orbit_edges=True,
                              nworkers=32):
     """
@@ -29,8 +29,9 @@ def make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf, sourceid,
 
         mdf: single row dataframe from CDIPS source catalog
 
-        suppdf: dataframe with LC statistics, Gaia xmatch info, CDIPS xmatch
-        info.  Columns include:
+        supprow: dataframe with LC statistics, Gaia xmatch info, CDIPS xmatch
+        info. Cut to match the sourceid of whatever object is getting the PDF
+        made.  Columns include:
 
            'lcobj', 'cat_mag', 'med_rm1', 'mad_rm1', 'mean_rm1', 'stdev_rm1',
            'ndet_rm1', 'med_sc_rm1', 'mad_sc_rm1', 'mean_sc_rm1',
@@ -49,6 +50,10 @@ def make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf, sourceid,
            'lum_val[47]', 'lum_percentile_lower[48]', 'lum_percentile_upper[49]'
            ...
            'cluster', 'ext_catalog_name', 'reference', 'source_id'
+
+        suppfulldf: as above, but the whole dataframe for all CDIPS sources
+        that got lightcurves for this sector. Useful for broader assessment of
+        the LC within the sample of cluster lightcurves.
     """
 
     hdul_sr = fits.open(tfa_sr_path)
@@ -96,7 +101,7 @@ def make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf, sourceid,
         # page 3 -- it's a QLP ripoff
         ##########
         fig, infodict = vp.transitcheckdetails(
-            tfasrmag, tfatime, tlsp, mdf, hdr, suppdf,
+            tfasrmag, tfatime, tlsp, mdf, hdr, supprow,
             obsd_midtimes=obsd_midtimes, figsize=(30,20)
         )
         pdf.savefig(fig)
@@ -108,6 +113,14 @@ def make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf, sourceid,
         fig = vp.scatter_increasing_ap_size(lc_sr, infodict=infodict,
                                             obsd_midtimes=obsd_midtimes,
                                             xlabel='BJDTDB', figsize=(30,20))
+        pdf.savefig(fig)
+        plt.close()
+
+        ##########
+        # page 5
+        ##########
+        fig = vp.cluster_membership_check(hdr, supprow, infodict, suppfulldf,
+                                          figsize=(30,16))
         pdf.savefig(fig)
         plt.close()
 
@@ -132,16 +145,16 @@ def make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf, sourceid,
         # * depth < 0.85
         # * rp > 6 R_jup = 67.25 R_earth. Based on 2x the limit given at 1Myr
         #   in Burrows+2001, figure 3.
-        # * SDE from TLS is < 12 (these will be unbelievable no matter what.
-        #   they might exist b/c TFA SR could have lowered overall SDE)
+        # * SNR from TLS is < 8 (these will be unbelievable no matter what.
+        #   they might exist b/c TFA SR could have lowered overall SDE/SNR)
         # * primary depth is >10%
         # * primary/secondary depth ratios from gaussian fitting in range of
         #   ~1.2-7, accounting for 1-sigma formal uncertainty in measurement
         ##########
         if (
-        (float(suppdf['ndet_tf2']) < 100) or
+        (float(supprow['ndet_tf2']) < 100) or
         (float(infodict['depth']) < 0.85) or
-        (float(infodict['sde']) < 12) or
+        (float(infodict['snr']) < 8) or
         (float(infodict['rp']) > 67.25) or
         ( (float(infodict['psdepthratio'] - infodict['psdepthratioerr']) > 1.2)
          &
@@ -161,7 +174,7 @@ def make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf, sourceid,
 
 
 
-def _get_suppdf(sourceid, supplementstatsdf):
+def _get_supprow(sourceid, supplementstatsdf):
 
     mdf = supplementstatsdf.loc[supplementstatsdf['lcobj']==sourceid]
 
@@ -212,11 +225,12 @@ def make_all_pdfs(tfa_sr_paths, lcbasedir, resultsdir, cdips_df,
             'vet_'+os.path.basename(tfa_sr_path).replace('.fits','.pdf')
         )
 
-        suppdf = _get_suppdf(sourceid, supplementstatsdf)
+        supprow = _get_supprow(sourceid, supplementstatsdf)
+        suppfulldf = supplementstatsdf
 
         if not os.path.exists(outpath) and not os.path.exists(nottransitpath):
             make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf,
-                                     sourceid, suppdf)
+                                     sourceid, supprow, suppfulldf)
         else:
             print('found {}, continue'.format(outpath))
 
@@ -228,12 +242,13 @@ def main(sectornum=6, cdips_cat_vnum=0.2):
         'vetting/'
         'sector-{}'.format(sectornum)
     )
-    if not os.path.exists(resultsdir):
-        os.mkdir(resultsdir)
-        os.mkdir(os.path.join(resultsdir,'pdfs'))
-        os.mkdir(os.path.join(resultsdir,'pkls'))
-        os.mkdir(os.path.join(resultsdir,'nottransitpdfs'))
-        os.mkdir(os.path.join(resultsdir,'nottransitpkls'))
+    dirs = [resultsdir, os.path.join(resultsdir,'pdfs'),
+            os.path.join(resultsdir,'pkls'),
+            os.path.join(resultsdir,'nottransitpdfs'),
+            os.path.join(resultsdir,'nottransitpkls')]
+    for _d in dirs:
+        if not os.path.exists(_d):
+            os.mkdir(_d)
 
     tfasrdir = ('/nfs/phtess2/ar0/TESS/PROJ/lbouma/'
                 'CDIPS_LCS/sector-{}_TFA_SR'.format(sectornum))
