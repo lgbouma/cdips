2a8747f4256c6e84e5d447eb6338bb95671315ce
lgbouma <bouma.luke@gmail.com>
2019-06-11 17:42:05 -0400

    classifixn update

--- drivers/classified_directory_to_csv.py
+++ drivers/classified_directory_to_csv.py
@@ -1,14 +1,16 @@
 """
 given a directory classified w/ TagSpaces tags, produce a CSV file with the
 classifications
+
+also produce CSV file with PC only classifications
 """
 import os
 from glob import glob
 import pandas as pd
 
-classified_dir = '/home/luke/classified/'
+classified_dir = '/home/luke/Dropbox/proj/cdips/results/vetting_classifications/sector_6_pdfs_VETTED_20190606/'
 outdir = '/home/luke/Dropbox/proj/cdips/results/vetting_classifications/'
-outname = '20190514_LGB_sector6_classifications_PC_cut.csv'
+outname = '20190611_LGB_sector6_classifications.csv'
 outpath = os.path.join(outdir, outname)
 
 pdfpaths = glob(os.path.join(classified_dir,'*pdf'))
@@ -24,3 +26,32 @@ if not os.path.exists(outpath):
     print('made {}'.format(outpath))
 else:
     print('found {}'.format(outpath))
+
+# PC cut
+
+outname = '20190611_LGB_sector6_classifications_PC_cut.csv'
+outpath = os.path.join(outdir, outname)
+
+outdf = df[df['Tags'].str.contains('PC', regex=False)]
+
+if not os.path.exists(outpath):
+    outdf.to_csv(outpath, index=False)
+    print('made {}'.format(outpath))
+else:
+    print('found {}'.format(outpath))
+
+
+# good cut
+
+outname = '20190611_LGB_sector6_classifications_good_cut.csv'
+outpath = os.path.join(outdir, outname)
+
+outdf = df[df['Tags'].str.contains('good', regex=False)]
+
+if not os.path.exists(outpath):
+    outdf.to_csv(outpath, index=False)
+    print('made {}'.format(outpath))
+else:
+    print('found {}'.format(outpath))
+
+
