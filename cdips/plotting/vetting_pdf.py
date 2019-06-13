5a1109c22c8e318f7f141616e791ad7a45c4c8c7
lgbouma <lgbouma@mit.edu>
2019-06-13 17:28:45 -0400

    oops

--- cdips/plotting/vetting_pdf.py
+++ cdips/plotting/vetting_pdf.py
@@ -1529,7 +1529,7 @@ def centroid_plots(c_obj, cd, hdr, _pfdf, toidf, figsize=(30,20),
                                      sizepix=sizepix, flip=False,
                                      cachedir='~/.astrobase/stamp-cache',
                                      verbose=True, savewcsheader=True)
-    except OSError, IndexError, TypeError as e:
+    except (OSError, IndexError, TypeError) as e:
         print('downloaded FITS appears to be corrupt, retrying...')
         dss, dss_hdr = skyview_stamp(ra, dec, survey='DSS2 Red',
                                      scaling='Linear', convolvewith=None,
