0d0e494f22579d21647e70450e00d98b9a8bf194
lgbouma <bouma.luke@gmail.com>
2019-06-17 16:54:22 -0400

    minor

--- drivers/classified_directory_to_csv.py
+++ drivers/classified_directory_to_csv.py
@@ -3,17 +3,29 @@ given a directory classified w/ TagSpaces tags, produce a CSV file with the
 classifications
 
 also produce CSV file with PC only classifications
+
+also produce directories with cuts to send to vetting collaborators
 """
-import os
+import os, shutil
 from glob import glob
 import pandas as pd
 
-classified_dir = '/home/luke/Dropbox/proj/cdips/results/vetting_classifications/sector_6_pdfs_VETTED_20190606/'
+##########################################
+# modify these
+sector = 6
+today = '20190616'
+classified_dir = '/home/luke/Dropbox/proj/cdips/results/vetting_classifications/sector_{}_{}_LGB_DONE/'.format(sector,today)
 outdir = '/home/luke/Dropbox/proj/cdips/results/vetting_classifications/'
-outname = '20190611_LGB_sector6_classifications.csv'
+outtocollabdir = '/home/luke/Dropbox/proj/cdips/results/vetting_classifications/{}_sector-{}_{}'
+outname = '{}_LGB_sector{}_classifications.csv'.format(today,sector)
+##########################################
+
 outpath = os.path.join(outdir, outname)
 
 pdfpaths = glob(os.path.join(classified_dir,'*pdf'))
+
+if not len(pdfpaths) > 1:
+    raise AssertionError('bad pdfpaths. no glob matches.')
 
 classes = [p.split('[')[1].replace('].pdf','') for p in pdfpaths]
 pdfnames = list(map(os.path.basename,
@@ -29,8 +41,8 @@ else:
 
 # PC cut
 
-outname = '20190611_LGB_sector6_classifications_PC_cut.csv'
-outpath = os.path.join(outdir, outname)
+pcoutname = outname.replace('.csv','_PC_cut.csv')
+outpath = os.path.join(outdir, pcoutname)
 
 outdf = df[df['Tags'].str.contains('PC', regex=False)]
 
@@ -40,11 +52,18 @@ if not os.path.exists(outpath):
 else:
     print('found {}'.format(outpath))
 
+collabdir = outtocollabdir.format(today, sector, 'PC_cut')
+for n in df['Name']:
+    shutil.copyfile(
+        '/home/luke/local/cdips/vetting/sector-{}/pdfs/vet_'.format(sector)+str(n)+'_llc.pdf',
+        os.path.join(collabdir,'vet_'+str(n)+'_llc.pdf')
+    )
+
 
 # good cut
 
-outname = '20190611_LGB_sector6_classifications_good_cut.csv'
-outpath = os.path.join(outdir, outname)
+goodoutname = outname.replace('.csv','_good_cut.csv')
+outpath = os.path.join(outdir, goodoutname)
 
 outdf = df[df['Tags'].str.contains('good', regex=False)]
 
@@ -53,5 +72,13 @@ if not os.path.exists(outpath):
     print('made {}'.format(outpath))
 else:
     print('found {}'.format(outpath))
+
+
+collabdir = outtocollabdir.format(today, sector, 'interesting_cut')
+for n in df['Name']:
+    shutil.copyfile(
+        '/home/luke/local/cdips/vetting/sector-{}/pdfs/vet_'.format(sector)+str(n)+'_llc.pdf',
+        os.path.join(collabdir,'vet_'+str(n)+'_llc.pdf')
+    )
 
 
