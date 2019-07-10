593ed48c813aa0bd84b6cbbfcb7ed6947ce3e3d1
lgbouma <bouma.luke@gmail.com>
2019-07-10 14:24:42 -0400

    add drivers/remove_select_reports.py

---
+++ drivers/remove_select_reports.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+import pandas as pd, numpy as np
+
+sector=6
+
+if sector==6:
+    date = '20190616'
+elif sector==7:
+    date = '20190618'
+
+csvpath = (
+    '/home/luke/Dropbox/proj/cdips/results/vetting_classifications/'
+    '{}_LGB_sector{}_classifications_PC_cut.csv'.format(date,sector)
+)
+
+df = pd.read_csv(csvpath)
+
+for n in np.array(df['Name']):
+
+    pdfname = n+'.pdf'
+
+    pdfdir = '/home/luke/local/cdips/vetting/sector-{}/pdfs/'.format(sector)
+
+    pdfpath = os.path.join(pdfdir, pdfname)
+
+    if os.path.exists(pdfpath):
+
+        os.remove(pdfpath)
+
+        print('removed {}'.format(pdfpath))
+
+    else:
+
+        print('didnt find & skipped {}'.format(pdfpath))
