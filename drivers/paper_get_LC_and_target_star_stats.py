54bca576015489e2cf47a9533f037128351f7b02
lgbouma <bouma.luke@gmail.com>
2019-07-27 21:03:32 -0400

    minor

--- drivers/paper_get_LC_and_target_star_stats.py
+++ drivers/paper_get_LC_and_target_star_stats.py
@@ -6,12 +6,13 @@ analyze stats of both cdips target stars, and cdips light curves.
 * how many are single-source claims?
 * how many are multi-source claims?
 
-python paper_get_LC_and_target_star_stats.py &> logs/get_LC_and_target_star_stats.log &
+python -u paper_get_LC_and_target_star_stats.py &> logs/get_LC_and_target_star_stats.log &
 """
 
 import pandas as pd, numpy as np
 from collections import Counter
 from cdips.utils import collect_cdips_lightcurves as ccl
+import os
 
 def main():
     print('#'*42)
@@ -21,18 +22,21 @@ def main():
     print('#'*42)
     paper_get_CDIPS_star_stats()
 
-def paper_get_CDIPS_LC_stats(sector=None):
+def paper_get_CDIPS_LC_stats(sector=None, ndet_cut=300, ncluster_cut=200):
 
     statdir = (
         '/nfs/phtess2/ar0/TESS/PROJ/lbouma/cdips/'+
         'results/cdips_lc_stats/sector-{}'.format(sector)
     )
 
-    statfile = os.path.join(statsdir, 'supplemented_cdips_lc_statistics.txt')
+    statfile = os.path.join(statdir, 'supplemented_cdips_lc_statistics.txt')
 
-    df = pd.read_csv(statfile)
+    df = pd.read_csv(statfile, sep=';')
 
-    get_CDIPS_stats(df)
+    df = df[df['ndet_rm2'] > ndet_cut]
+
+    print('Sector {}'.format(sector))
+    get_CDIPS_stats(df, ncluster_cut=ncluster_cut)
 
 
 def paper_get_CDIPS_star_stats():
@@ -42,7 +46,7 @@ def paper_get_CDIPS_star_stats():
     get_CDIPS_stats(df)
 
 
-def get_CDIPS_stats(df):
+def get_CDIPS_stats(df, ncluster_cut=100):
     """
     df (pd.DataFrame): dataframe with stats information from either the
     (publication-level) target star catalog (ccl.get_cdips_catalog), or the
@@ -53,7 +57,7 @@ def get_CDIPS_stats(df):
 
     ucname = np.array(df['unique_cluster_name'])
     cname_cnt = Counter(ucname)
-    cname_mostcommon = cname_cnt.most_common(n=30)
+    cname_mostcommon = cname_cnt.most_common(n=ncluster_cut)
 
     refs = np.array(df['reference'])
     cnt = Counter(refs)
@@ -69,7 +73,7 @@ def get_CDIPS_stats(df):
     """
     Total number of stars: {n_stars}
 
-    Top 30 most common unique cluster names are:
+    Top {ncluster_cut} most common unique cluster names are:
     {cname_mostcommon}
 
     Top 20 most common references are:
@@ -80,6 +84,7 @@ def get_CDIPS_stats(df):
     There are {n_mult} claims from multiple sources.
     """.format(
         n_stars=n_stars,
+        ncluster_cut=ncluster_cut,
         cname_mostcommon=repr(cname_mostcommon),
         mostcommon=repr(mostcommon),
         n_single=n_single,
