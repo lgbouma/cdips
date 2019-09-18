02ec355e92567de346f2ad3afc250d11864f9d24
lgbouma <bouma.luke@gmail.com>
2019-09-18 10:15:08 -0400

    mv configparser management to utils.pipelineutils

---
+++ cdips/utils/pipelineutils.py
@@ -0,0 +1,46 @@
+import configparser
+
+def save_status(status_file, section, state_vars):
+    """
+    Save pipeline status
+
+    Args:
+
+        status_file (string): name of output file
+
+        section (string): name of section to write
+
+        state_vars (dict): dictionary of all options to populate the specified
+        section
+    """
+
+    config = configparser.RawConfigParser()
+
+    if os.path.isfile(status_file):
+        config.read(status_file)
+
+    if not config.has_section(section):
+        config.add_section(section)
+
+    for key, val in state_vars.items():
+        config.set(section, key, val)
+
+    with open(status_file, 'w') as f:
+        config.write(f)
+
+
+def load_status(status_file):
+    """
+    Load pipeline status
+
+    Args:
+        status_file (string): name of configparser file
+
+    Returns:
+        configparser.RawConfigParser
+    """
+
+    config = configparser.RawConfigParser()
+    gl = config.read(status_file)
+
+    return config
--- drivers/fit_models_to_gold.py
+++ drivers/fit_models_to_gold.py
@@ -30,6 +30,7 @@ from cdips.lcproc import mask_orbit_edges as moe
 from cdips.plotting import vetting_pdf as vp
 from cdips.utils import collect_cdips_lightcurves as ccl
 from cdips.utils import today_YYYYMMDD
+from cdips.utils.pipelineutils import save_status, load_status
 
 import make_vetting_multipg_pdf as mvp
 from astrobase import imageutils as iu
@@ -433,52 +434,6 @@ def _fit_transit_model_single_sector(tfa_sr_path, lcpath, outpath, mdf,
     else:
         print('WRN! {} did not converge, after {} steps. MUST MANUALLY FIX.'.
               format(identifier, status['n_steps_run']))
-
-
-def save_status(status_file, section, state_vars):
-    """
-    Save pipeline status
-
-    Args:
-
-        status_file (string): name of output file
-
-        section (string): name of section to write
-
-        state_vars (dict): dictionary of all options to populate the specified
-        section
-    """
-
-    config = configparser.RawConfigParser()
-
-    if os.path.isfile(status_file):
-        config.read(status_file)
-
-    if not config.has_section(section):
-        config.add_section(section)
-
-    for key, val in state_vars.items():
-        config.set(section, key, val)
-
-    with open(status_file, 'w') as f:
-        config.write(f)
-
-
-def load_status(status_file):
-    """
-    Load pipeline status
-
-    Args:
-        status_file (string): name of configparser file
-
-    Returns:
-        configparser.RawConfigParser
-    """
-
-    config = configparser.RawConfigParser()
-    gl = config.read(status_file)
-
-    return config
 
 
 def str2bool(v):
