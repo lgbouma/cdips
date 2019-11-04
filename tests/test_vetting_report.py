4a6024544279e245332507ebcfc6ca841b41b63b
lgbouma <bouma.luke@gmail.com>
2019-11-04 11:56:31 -0500

    [WIP] vetting reports: add test script

---
+++ tests/test_vetting_report.py
@@ -0,0 +1,36 @@
+import os
+
+from cdips.vetting import (
+    initialize_vetting_report_information as ivri,
+    make_all_vetting_reports as mavp
+)
+
+
+def test_vetting_report(sector=None, cdips_cat_vnum=None):
+
+    outdir = 'test_vetting_reports'
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    (tfa_sr_paths, lcbasedir, resultsdir,
+    cddf, supplementstatsdf, pfdf, toidf,
+    k13_notes_df, sector) = (
+            ivri.initialize_vetting_report_information(sector, cdips_cat_vnum,
+                                                       baseresultsdir=outdir)
+    )
+
+    id_to_test = '5256502966294604800'
+
+
+    tfa_sr_paths = [f for f in tfa_sr_paths if id_to_test in f]
+
+    mavp.make_all_vetting_reports(
+        tfa_sr_paths, lcbasedir, resultsdir, cddf, supplementstatsdf, pfdf,
+        toidf, k13_notes_df, sector=sector
+    )
+
+
+if __name__ == "__main__":
+
+    test_vetting_report(sector=9, cdips_cat_vnum=0.4)
+
