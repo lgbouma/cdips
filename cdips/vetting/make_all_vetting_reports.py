747beefd58931104d2a52a8113bf47411b9a2543
lgbouma <bouma.luke@gmail.com>
2019-11-04 11:25:52 -0500

    vetting reports: move many-report generator to pkg

---
+++ cdips/vetting/make_all_vetting_reports.py
@@ -0,0 +1,72 @@
+import os
+import numpy as np, pandas as pd
+from astropy.io import fits
+
+def _get_supprow(sourceid, supplementstatsdf):
+
+    mdf = supplementstatsdf.loc[supplementstatsdf['lcobj']==sourceid]
+
+    return mdf
+
+
+def make_all_vetting_reports(tfa_sr_paths, lcbasedir, resultsdir, cdips_df,
+                             supplementstatsdf, pfdf, toidf, k13_notes_df,
+                             sector=6, cdipsvnum=1):
+
+    for tfa_sr_path in tfa_sr_paths:
+
+        sourceid = int(tfa_sr_path.split('gaiatwo')[1].split('-')[0].lstrip('0'))
+        mdf = cdips_df[cdips_df['source_id']==sourceid]
+        if len(mdf) != 1:
+            errmsg = 'expected exactly 1 source match in CDIPS cat'
+            raise AssertionError(errmsg)
+
+        hdul = fits.open(tfa_sr_path)
+        hdr = hdul[0].header
+        cam, ccd = hdr['CAMERA'], hdr['CCD']
+        hdul.close()
+
+        lcname = (
+            'hlsp_cdips_tess_ffi_'
+            'gaiatwo{zsourceid}-{zsector}-cam{cam}-ccd{ccd}_'
+            'tess_v{zcdipsvnum}_llc.fits'
+        ).format(
+            cam=cam,
+            ccd=ccd,
+            zsourceid=str(sourceid).zfill(22),
+            zsector=str(sector).zfill(4),
+            zcdipsvnum=str(cdipsvnum).zfill(2)
+        )
+
+        lcpath = os.path.join(
+            lcbasedir,
+            'cam{}_ccd{}'.format(cam, ccd),
+            lcname
+        )
+
+        # logic: even if you know it's nottransit, it's a TCE. therefore, the
+        # pdf will be made. i just dont want to have to look at it. put it in a
+        # separate directory.
+        outpath = os.path.join(
+            resultsdir,'pdfs',
+            'vet_'+os.path.basename(tfa_sr_path).replace('.fits','.pdf')
+        )
+        nottransitpath = os.path.join(
+            resultsdir,'nottransitpdfs',
+            'vet_'+os.path.basename(tfa_sr_path).replace('.fits','.pdf')
+        )
+
+        supprow = _get_supprow(sourceid, supplementstatsdf)
+        suppfulldf = supplementstatsdf
+
+        pfrow = pfdf.loc[pfdf['source_id']==sourceid]
+        if len(pfrow) != 1:
+            errmsg = 'expected exactly 1 source match in period find df'
+            raise AssertionError(errmsg)
+
+        if not os.path.exists(outpath) and not os.path.exists(nottransitpath):
+            mvmp.make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf,
+                                          sourceid, supprow, suppfulldf, pfdf,
+                                          pfrow, toidf, sector, k13_notes_df)
+        else:
+            print('found {}, continue'.format(outpath))
--- drivers/make_all_vetting_reports.py
+++ drivers/make_all_vetting_reports.py
@@ -13,9 +13,8 @@ from matplotlib.backends.backend_pdf import PdfPages
 from cdips.plotting import vetting_pdf as vp
 from cdips.utils import collect_cdips_lightcurves as ccl
 from cdips.vetting import (
-    centroid_analysis as cdva,
-    make_vetting_multipg_pdf as mvmp,
-    initialize_vetting_report_information as ivri
+    initialize_vetting_report_information as ivri,
+    make_all_vetting_reports as mavp
 )
 
 from numpy import array as nparr
@@ -27,76 +26,6 @@ from astropy import units as u
 from astroquery.vizier import Vizier
 
 
-def _get_supprow(sourceid, supplementstatsdf):
-
-    mdf = supplementstatsdf.loc[supplementstatsdf['lcobj']==sourceid]
-
-    return mdf
-
-
-def make_all_vetting_reports(tfa_sr_paths, lcbasedir, resultsdir, cdips_df,
-                             supplementstatsdf, pfdf, toidf, k13_notes_df,
-                             sector=6, cdipsvnum=1):
-
-    for tfa_sr_path in tfa_sr_paths:
-
-        sourceid = int(tfa_sr_path.split('gaiatwo')[1].split('-')[0].lstrip('0'))
-        mdf = cdips_df[cdips_df['source_id']==sourceid]
-        if len(mdf) != 1:
-            errmsg = 'expected exactly 1 source match in CDIPS cat'
-            raise AssertionError(errmsg)
-
-        hdul = fits.open(tfa_sr_path)
-        hdr = hdul[0].header
-        cam, ccd = hdr['CAMERA'], hdr['CCD']
-        hdul.close()
-
-        lcname = (
-            'hlsp_cdips_tess_ffi_'
-            'gaiatwo{zsourceid}-{zsector}-cam{cam}-ccd{ccd}_'
-            'tess_v{zcdipsvnum}_llc.fits'
-        ).format(
-            cam=cam,
-            ccd=ccd,
-            zsourceid=str(sourceid).zfill(22),
-            zsector=str(sector).zfill(4),
-            zcdipsvnum=str(cdipsvnum).zfill(2)
-        )
-
-        lcpath = os.path.join(
-            lcbasedir,
-            'cam{}_ccd{}'.format(cam, ccd),
-            lcname
-        )
-
-        # logic: even if you know it's nottransit, it's a TCE. therefore, the
-        # pdf will be made. i just dont want to have to look at it. put it in a
-        # separate directory.
-        outpath = os.path.join(
-            resultsdir,'pdfs',
-            'vet_'+os.path.basename(tfa_sr_path).replace('.fits','.pdf')
-        )
-        nottransitpath = os.path.join(
-            resultsdir,'nottransitpdfs',
-            'vet_'+os.path.basename(tfa_sr_path).replace('.fits','.pdf')
-        )
-
-        supprow = _get_supprow(sourceid, supplementstatsdf)
-        suppfulldf = supplementstatsdf
-
-        pfrow = pfdf.loc[pfdf['source_id']==sourceid]
-        if len(pfrow) != 1:
-            errmsg = 'expected exactly 1 source match in period find df'
-            raise AssertionError(errmsg)
-
-        if not os.path.exists(outpath) and not os.path.exists(nottransitpath):
-            mvmp.make_vetting_multipg_pdf(tfa_sr_path, lcpath, outpath, mdf,
-                                          sourceid, supprow, suppfulldf, pfdf,
-                                          pfrow, toidf, sector, k13_notes_df)
-        else:
-            print('found {}, continue'.format(outpath))
-
-
 def main(sector=None, cdips_cat_vnum=None):
 
     (tfa_sr_paths, lcbasedir, resultsdir,
@@ -105,9 +34,9 @@ def main(sector=None, cdips_cat_vnum=None):
             ivri.initialize_vetting_report_information(sector, cdips_cat_vnum)
     )
 
-    make_all_vetting_reports(tfa_sr_paths, lcbasedir, resultsdir, cddf,
-                             supplementstatsdf, pfdf, toidf, k13_notes_df,
-                             sector=sector)
+    mavp.make_all_vetting_reports(tfa_sr_paths, lcbasedir, resultsdir, cddf,
+                                  supplementstatsdf, pfdf, toidf, k13_notes_df,
+                                  sector=sector)
 
 
 if __name__ == "__main__":
