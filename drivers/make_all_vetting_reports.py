4663fb2599ec18cf669c692e5a560a09cf876706
lgbouma <bouma.luke@gmail.com>
2019-11-04 11:28:05 -0500

    vetting report driver cleanup

--- drivers/make_all_vetting_reports.py
+++ drivers/make_all_vetting_reports.py
@@ -4,27 +4,10 @@ Make multipage PDFs needed to vet CDIPS objects of interest.
 $ python -u make_all_vetting_reports.py &> logs/vetting_pdf.log &
 """
 
-from glob import glob
-import datetime, os, pickle, shutil
-import numpy as np, pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
-
-from cdips.plotting import vetting_pdf as vp
-from cdips.utils import collect_cdips_lightcurves as ccl
 from cdips.vetting import (
     initialize_vetting_report_information as ivri,
     make_all_vetting_reports as mavp
 )
-
-from numpy import array as nparr
-from astropy.io import fits
-from datetime import datetime
-
-from astropy.coordinates import SkyCoord
-from astropy import units as u
-from astroquery.vizier import Vizier
-
 
 def main(sector=None, cdips_cat_vnum=None):
 
@@ -34,9 +17,10 @@ def main(sector=None, cdips_cat_vnum=None):
             ivri.initialize_vetting_report_information(sector, cdips_cat_vnum)
     )
 
-    mavp.make_all_vetting_reports(tfa_sr_paths, lcbasedir, resultsdir, cddf,
-                                  supplementstatsdf, pfdf, toidf, k13_notes_df,
-                                  sector=sector)
+    mavp.make_all_vetting_reports(
+        tfa_sr_paths, lcbasedir, resultsdir, cddf, supplementstatsdf, pfdf,
+        toidf, k13_notes_df, sector=sector
+    )
 
 
 if __name__ == "__main__":
