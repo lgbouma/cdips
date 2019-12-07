843939639fee06b42655e00969acc591fab17856
lgbouma <bouma.luke@gmail.com>
2019-12-07 11:30:15 -0500

    minor

--- cdips/utils/catalogs.py
+++ cdips/utils/catalogs.py
@@ -42,12 +42,14 @@ def get_cdips_pub_catalog(ver=0.4):
 
 
 def get_toi_catalog(ver='2019-12-05'):
+    # a misnomer: really, the TOI-plus catalog.
 
     dir_d = {
         'brik':'/home/luke/Dropbox/proj/cdips/data/',
         'phtess1':'/nfs/phtess1/ar1/TESS/PROJ/lbouma/',
         'phtess2':'/nfs/phtess1/ar1/TESS/PROJ/lbouma/',
-        'phn12':'/nfs/phtess1/ar1/TESS/PROJ/lbouma/'
+        'phn12':'/nfs/phtess1/ar1/TESS/PROJ/lbouma/',
+        'ast1607-astro':'/Users/luke/Dropbox/proj/cdips/data/'
     }
 
     toi_stars_dir = dir_d[socket.gethostname()]
