90072f5ef77a57448f53b18b3ca55004d8344138
lgbouma <bouma.luke@gmail.com>
2019-12-11 12:23:18 -0500

    mv ticid_to_toiid from cdips_followup

--- cdips/utils/catalogs.py
+++ cdips/utils/catalogs.py
@@ -131,3 +131,25 @@ def get_exofop_toi_catalog(ver='2019-12-07', returnpath=False):
         return df
     if returnpath:
         return toi_stars_path
+
+
+def ticid_to_toiid(tic_id):
+
+    assert isinstance(tic_id, str)
+
+    if tic_id == '772008799':
+        # exofop-tess has wrong ticid for TOI 1014.
+        return '1014.01'
+
+    toidf = get_exofop_toi_catalog()
+
+    sel = toidf['TIC ID'].astype(str) == tic_id
+
+    try:
+        toi_id = str(toidf[sel]['TOI'].iloc[0])
+        if len(toi_id) > 1:
+            return str(toi_id)
+        else:
+            return None
+    except:
+        return None
