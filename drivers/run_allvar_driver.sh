fca2202a16080516ce139c2998bf3c3fa5de5f58
lgbouma <bouma.luke@gmail.com>
2020-11-18 16:18:26 -0800

    ditto

--- drivers/run_allvar_driver.sh
+++ drivers/run_allvar_driver.sh
@@ -2,7 +2,10 @@
 # $ run_allvar_driver.sh &
 # Reason:
 # This program has memory bloat; this approach manually restarts it.
+# The approach is pretty bad; a manual process kill (e.g., `killall bash`, or
+# the manual ID) is needed afterward.
 while true;
 do
-  python -u do_allvariable_report_making.py &> logs/ngc2516_core_plus_halo.log ; wait
+  echo 'NEW LOOP BEGINS' `date` >> logs/ngc2516_core_plus_halo.log
+  python -u do_allvariable_report_making.py &>> logs/ngc2516_core_plus_halo.log ; wait
 done
