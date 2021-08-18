7e27cfc978ce243723861bf8b8962755c4423271
lgbouma <bouma.luke@gmail.com>
2021-08-18 13:53:12 -0400

    clean up tests

--- tests/test_reformat_headers.py
+++ tests/test_reformat_headers.py
@@ -2,9 +2,10 @@
 Test the "reformat_headers" function in lcproc.reformat_lcs_for_mast, to make
 sure the PCA calculation looks OK.
 """
-import numpy as np
+import numpy as np, matplotlib.pyplot as plt
 import os
 from glob import glob
+from astropy.io import fits
 from astrobase import imageutils as iu
 from cdips.lcproc import detrend as dtr
 from cdips.lcproc import reformat_lcs_for_mast as rlm
@@ -33,3 +34,17 @@ OC_MG_CAT_ver = 0.6
 rlm.reformat_headers(lcpaths, camccddir, sector, cdipsvnum, OC_MG_CAT_ver,
                      eigveclist=eigveclist, n_comp_df=n_comp_df)
 
+# trust, but verify
+lcpaths = glob(os.path.join(camccddir, 'hlsp*fits'))
+np.random.seed(42)
+lcpath = np.random.choice(lcpaths, 1, replace=False)
+hl = fits.open(lcpath[0])
+fig, ax = plt.subplots(nrows=3, figsize=(12,8), sharex=True)
+cols = ['IRM','TFA','PCA']
+for i, c in enumerate(cols):
+    ax[i].scatter(hl[1].data['TMID_BJD'], hl[1].data[f'{c}1'], s=1, c='k')
+    ax[i].set_ylabel(f'{c}1')
+ax[-1].set_xlabel('tmidbjd')
+ax[0].set_title(lcpath, size='xx-small')
+plt.savefig('temp.png')
+hl.close()
