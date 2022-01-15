lgbouma/cdips

commit fab132e97afcc18373d7ef98be9f3ebcc7b0b53e

minor tweaks

cdips/vetting/initialize_neighborhood_information.py
@@ -385,11 +385,13 @@ def get_group_and_neighborhood_information(
     print('bounding by {} stdevns'.format(n_std))
 
     for param in params:
-        bounds[param+'_upper'] = (
-            group_df_dr2[param].mean() + n_std*group_df_dr2[param].std()
+        bounds[param+'_upper'] = np.minimum(
+            group_df_dr2[param].mean() + n_std*group_df_dr2[param].std(),
+            360-1e-5
         )
-        bounds[param+'_lower'] = (
-            group_df_dr2[param].mean() - n_std*group_df_dr2[param].std()
+        bounds[param+'_lower'] = np.maximum(
+            group_df_dr2[param].mean() - n_std*group_df_dr2[param].std(),
+            1e-5
         )
 
     if bounds['parallax_lower'] < 0:

cdips/vetting/initialize_vetting_report_information.py
@@ -6,7 +6,7 @@ import os
 import numpy as np, pandas as pd
 
 from cdips.utils import collect_cdips_lightcurves as ccl
-from cdips.utils.catalogs import get_toi_catalog
+from cdips.utils.catalogs import get_toi_catalog, get_exofop_toi_catalog
 from astroquery.vizier import Vizier
 
 def initialize_vetting_report_information(
@@ -79,7 +79,10 @@ def initialize_vetting_report_information(
         print('WRN! Did not find pfpath {}'.format(pfpath))
         pfdf = None
 
-    toidf = get_toi_catalog()
+    try:
+        toidf = get_toi_catalog()
+    except:
+        toidf = get_exofop_toi_catalog()
 
     Vizier.ROW_LIMIT = -1
     catalog_list = Vizier.find_catalogs('J/A+A/558/A53')

commit 76305745678ec69260112bede9f769871a1e8016

nbhd acquisition: fix bug in bounds definition
Introduced by fab132e97afcc18373d7ef98be9f3ebcc7b0b53e

cdips/vetting/initialize_neighborhood_information.py L392

Affected "page 6" of vetting reports for queries with neighborhood
bounds below 0 degrees declination after Aug 10, 2021.

So: based on https://tess.mit.edu/wp-content/uploads/PM_year2_cel.png
this doesn't enough of the Cycle 2 reports for it to be a problem.  It
would only affect one CCD of Sector 23!

cdips/vetting/initialize_neighborhood_information.py
@@ -139,12 +139,16 @@ def get_neighborhood_information(
                 float(target_df[param]) - mult*n_std*float(target_df[param + '_error'])
             )
 
-        if bounds['ra_upper'] > 360:
-            bounds['ra_upper'] = 359.99
-        if bounds['ra_lower'] < 0:
-            bounds['ra_lower'] = 0
         if bounds['parallax_lower'] < 0:
             bounds['parallax_lower'] = 0
+        if bounds['ra_upper'] > 360:
+            bounds['ra_upper'] = 359.999
+        if bounds['ra_lower'] < 0:
+            bounds['ra_lower'] = 0
+        if bounds['dec_upper'] > 90:
+            bounds['dec_upper'] = 89.999
+        if bounds['dec_lower'] < -90:
+            bounds['dec_lower'] = -89.999
 
         n_max = int(1e4)
 
@@ -346,17 +350,23 @@ def get_group_and_neighborhood_information(
     LOGINFO(f'bounding by {n_std} stdevns')
 
     for param in params:
-        bounds[param+'_upper'] = np.minimum(
-            group_df_dr2[param].mean() + n_std*group_df_dr2[param].std(),
-            360-1e-5
+        bounds[param+'_upper'] = (
+            group_df_dr2[param].mean() + n_std*group_df_dr2[param].std()
         )
-        bounds[param+'_lower'] = np.maximum(
-            group_df_dr2[param].mean() - n_std*group_df_dr2[param].std(),
-            1e-5
+        bounds[param+'_lower'] = (
+            group_df_dr2[param].mean() - n_std*group_df_dr2[param].std()
         )
 
     if bounds['parallax_lower'] < 0:
         bounds['parallax_lower'] = 0
+    if bounds['ra_upper'] > 360:
+        bounds['ra_upper'] = 359.999
+    if bounds['ra_lower'] < 0:
+        bounds['ra_lower'] = 0
+    if bounds['dec_upper'] > 90:
+        bounds['dec_upper'] = 89.999
+    if bounds['dec_lower'] < -90:
+        bounds['dec_lower'] = -89.999
 
     assert bounds['ra_upper'] < 360
     assert bounds['ra_lower'] > 0

tests/test_nbhd_plot.py
@@ -23,6 +23,12 @@ def test_nbhd_plot(source_id, sector, cdips_cat_vnum=0.6,
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
+    outpath = os.path.join(outdir, f'{source_id}_nbhd_plot.png')
+
+    if os.path.exists(outpath):
+        print(f'Found {outpath}! continue.')
+        return 1
+
     (lc_paths, lcbasedir, resultsdir, cdips_df, supplementstatsdf, pfdf, toidf,
      sector) = (
             ivri.initialize_vetting_report_information(
@@ -82,7 +88,6 @@ def test_nbhd_plot(source_id, sector, cdips_cat_vnum=0.6,
 
         raise NotImplementedError
 
-    outpath = os.path.join(outdir, '{}_nbhd_plot.png'.format(source_id))
     fig.savefig(outpath)
     print('made {}'.format(outpath))
 