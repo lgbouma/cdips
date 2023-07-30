c2b91c6e0909350e4a0fab787f7c83565d792634
lgbouma <bouma.luke@gmail.com>
2023-07-30 07:46:42 -0400

    lc_to_hlspformat: minor

--- drivers/lc_to_hlspformat.py
+++ drivers/lc_to_hlspformat.py
@@ -9,7 +9,7 @@ After running, period-finding can be run on the light curves for which it is
 expected to be worthwhile, in `do_initial_period_finding`.
 
 USAGE:
-    python -u lc_to_hlspformat.py &> logs/s14_to_hlsp.log &
+    (trex_37 environment) python -u lc_to_hlspformat.py &> logs/s14_to_hlsp.log &
 """
 
 import os, shutil
@@ -39,6 +39,8 @@ def main():
     ccds = [1,2,3,4]
     OC_MG_CAT_ver = 0.6
     cdipsvnum = 1
+
+    make_symlinks = 1
     ##########################################
 
     nworkers = mp.cpu_count()
@@ -49,12 +51,14 @@ def main():
     # turn cdips-pipeline light curves to HLSP light curves
     if len(lcpaths) == 0 or overwrite:
         tlml.trex_lc_to_mast_lc(
-            sectors=[sector], cams=cams, ccds=ccds, make_symlinks=1,
+            sectors=[sector], cams=cams, ccds=ccds, make_symlinks=make_symlinks,
             reformat_lcs=1, OC_MG_CAT_ver=OC_MG_CAT_ver, cdipsvnum=cdipsvnum,
             outdir=outdir, symlinkdir=symlinkdir, lcbasedir=lcbasedir
         )
     else:
         print('found {} HLSP LCs; wont reformat'.format(len(lcpaths)))
+
+    print('🎉 reformatted!')
 
     # get stats, make the supp data file, print out the metadata, and move
     # allnan light curves
